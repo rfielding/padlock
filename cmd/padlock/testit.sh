d45105bc442fa2af481b991ea100a759c9f1477a
Robert Fielding <robertfielding@Roberts-Mini.home>
2022-03-27 00:32:18 -0400

    use two different users to see what they can unlock

--- cmd/padlock/testit.sh
+++ cmd/padlock/testit.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Create Alice and Bob with the CA
+go run main.go --priv farkfark --facts ./requests/bob.request.json > ./users/bob.cert.json
 go run main.go --priv farkfark --facts ./requests/alice.request.json > ./users/alice.cert.json
+
+# Alice creates a padlock (should be same result, as long as same signer
 go run main.go --cert ./users/alice.cert.json --blueprint ./blueprints/isAdultCit.blueprint.json > ./padlocks/isAdultCit.padlock.json
+
+echo alice private Certificate
+cat ./users/alice.cert.json
+echo bob private Certificate
+cat ./users/bob.cert.json
+
+echo alice Unlocks
 go run main.go --cert ./users/alice.cert.json --padlock ./padlocks/isAdultCit.padlock.json
+echo bob Unlocks
+go run main.go --cert ./users/bob.cert.json --padlock ./padlocks/isAdultCit.padlock.json
