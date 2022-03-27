d6604a90a2fa17b0845a1f159dca5fbfedd3b16c
Robert Fielding <robertfielding@Roberts-Mini.home>
2022-03-26 22:08:42 -0400

    refactor

--- cmd/padlock/main.go
+++ cmd/padlock/main.go
@@ -10,6 +10,92 @@ import (
 	"flag"
 )
 
+func unlockPadlock(certFile, padlockFile *string) {
+	certBytes, err := ioutil.ReadFile(*certFile)
+	if err != nil {
+		panic(fmt.Sprintf("need a cert to know which public key to sign to: %v", err))
+	}
+	var cert abe.Certificate
+	err = json.Unmarshal(certBytes, &cert)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot unmarshal certificate: %v", err))
+	}
+
+	padlockBytes, err := ioutil.ReadFile(*padlockFile)
+	if err != nil {
+		panic(fmt.Sprintf("need a padlock to unlock: %v", err))
+	}
+	var e abe.Spec
+	err = json.Unmarshal(padlockBytes, &e)
+	if err != nil {
+		panic(fmt.Sprintf("Error decoding spec: %v", err))
+	}
+
+	granted, err := e.Unlock(cert)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", abe.AsJson(granted))
+}
+
+func issueCert(privStr, factsFile *string) {
+	// Set up the CA
+	priv := abe.Hs(*privStr)
+	factsBytes, err := ioutil.ReadFile(*factsFile)
+	if err != nil {
+		panic(fmt.Sprintf("cannot open facts file %s: %v", *factsFile, err))
+	}
+	var facts []string
+	err = json.Unmarshal(factsBytes, &facts)
+	if err != nil {
+		panic(fmt.Sprintf("cannot parse facts json: %v", err))
+	}
+	// Create a certificate
+	alice, err := abe.Issue(
+		priv,
+		facts,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", abe.AsJson(alice))
+}
+
+func createPadlock(certFile, blueprintFile, readKeyStr, writeKeyStr *string) {
+	blueprint, err := ioutil.ReadFile(*blueprintFile)
+	if err != nil {
+		panic(fmt.Sprintf("cannot open blueprints file %s: %v", *blueprintFile, err))
+	}
+	W, err := hex.DecodeString(*writeKeyStr)
+	if err != nil {
+		panic("cannot decode write key")
+	}
+	R, err := hex.DecodeString(*readKeyStr)
+	if err != nil {
+		panic("cannot decode read key")
+	}
+	certBytes, err := ioutil.ReadFile(*certFile)
+	if err != nil {
+		panic(fmt.Sprintf("need a cert to know which public key to sign to: %v", err))
+	}
+	var cert abe.Certificate
+	err = json.Unmarshal(certBytes, &cert)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot unmarshal certificate: %v", err))
+	}
+
+	pub, err := cert.Cert()
+	if err != nil {
+		panic(fmt.Sprintf("Cannot unmarshal certificate signer: %v", err))
+	}
+	keyMap := map[string][]byte{"Write": W[:], "Read": R[:]}
+	e, err := abe.AsSpec(string(blueprint), pub, keyMap)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", abe.AsJson(e))
+}
+
 func main() {
   padlockFile := flag.String("padlock", "", "required to unlock: padlock file to unlock with cert")
 	blueprintFile := flag.String("blueprint", "./blueprints/isAdultCit.padlock.json", "required to make padlock: blueprint file to generate a padlock")
@@ -25,117 +111,19 @@ func main() {
 
 	// Unlock a padlock
 	if len(*padlockFile) > 0 {
-		certBytes, err := ioutil.ReadFile(*certFile)
-		if err != nil {
-			panic(fmt.Sprintf("need a cert to know which public key to sign to: %v", err))
-		}
-		var cert abe.Certificate
-		err = json.Unmarshal(certBytes, &cert)
-		if err != nil {
-			panic(fmt.Sprintf("Cannot unmarshal certificate: %v", err))
-		}
-
-		padlockBytes, err := ioutil.ReadFile(*padlockFile)
-		if err != nil {
-			panic(fmt.Sprintf("need a padlock to unlock: %v", err))
-		}
-		var e abe.Spec
-		err = json.Unmarshal(padlockBytes, &e)
-		if err != nil {
-			panic(fmt.Sprintf("Error decoding spec: %v", err))
-		}
-
-		granted, err := e.Unlock(cert)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", abe.AsJson(granted))
+		unlockPadlock(certFile, padlockFile)
 		return
 	}
 
 	// We must be issuing a cert
 	if len(*privStr) > 0 {
-		// Set up the CA
-		priv := abe.Hs(*privStr)
-		factsBytes, err := ioutil.ReadFile(*factsFile)
-		if err != nil {
-			panic(fmt.Sprintf("cannot open facts file %s: %v", *factsFile, err))
-		}
-		var facts []string
-		err = json.Unmarshal(factsBytes, &facts)
-		if err != nil {
-			panic(fmt.Sprintf("cannot parse facts json: %v", err))
-		}
-		// Create a certificate
-		alice, err := abe.Issue(
-			priv,
-			facts,
-		)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", abe.AsJson(alice))
+		issueCert(privStr, factsFile	)
 		return
 	}
 
 	// Create a padlock from a certificate and a blueprint
 	if len(*certFile) > 0 {
-		blueprint, err := ioutil.ReadFile(*blueprintFile)
-		if err != nil {
-			panic(fmt.Sprintf("cannot open blueprints file %s: %v", *blueprintFile, err))
-		}
-		W, err := hex.DecodeString(*writeKeyStr)
-		if err != nil {
-			panic("cannot decode write key")
-		}
-		R, err := hex.DecodeString(*readKeyStr)
-		if err != nil {
-			panic("cannot decode read key")
-		}
-		certBytes, err := ioutil.ReadFile(*certFile)
-		if err != nil {
-			panic(fmt.Sprintf("need a cert to know which public key to sign to: %v", err))
-		}
-		var cert abe.Certificate
-		err = json.Unmarshal(certBytes, &cert)
-		if err != nil {
-			panic(fmt.Sprintf("Cannot unmarshal certificate: %v", err))
-		}
-
-		pub, err := cert.Cert()
-		if err != nil {
-			panic(fmt.Sprintf("Cannot unmarshal certificate signer: %v", err))
-		}
-		keyMap := map[string][]byte{"Write": W[:], "Read": R[:]}
-		e, err := abe.AsSpec(string(blueprint), pub, keyMap)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", abe.AsJson(e))
+		createPadlock(certFile, blueprintFile, readKeyStr, writeKeyStr)
 		return
 	}
-
-/*
-
-	_ = *certFile
-
-
-
-	// Create the padlock
-
-
-	// Attempt an unlock
-	granted, err := e.Unlock(alice)
-	if err != nil {
-		panic(err)
-	}
-	for k, v := range granted {
-		fmt.Printf("alice granted %s: %s\n", k, hex.EncodeToString(v))
-		fmt.Printf("alice expected %s: %s\n", k, hex.EncodeToString(keyMap[k]))
-	}
-	*/
-	_ = blueprintFile
-	_ = certFile
-	_ = readKeyStr
-	_ = writeKeyStr
 }
